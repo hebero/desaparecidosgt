c41c546a2f5cc5fe72c6dfe323f323455287ee70
Ubuntu <ubuntu@ip-172-31-23-235.us-east-2.compute.internal>
2020-10-17 21:41:48 +0000

    Evita a que caiga la ejecución del script capturando el error en la llamada principal

--- app/autoreply.py
+++ app/autoreply.py
@@ -6,39 +6,57 @@ import time
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
-keywords = ["viendo", "estaba", "ojo"]
+history = set()
+keywords = ["viendo", "estaba", "ojo", "iniciativa"]
 def reply_mentions(api, since_id):
     logger.info("new metion")
     new_since_id = since_id
     for tweet in tweepy.Cursor(api.mentions_timeline,
      since_id=since_id).items():
+        time.sleep(10)
         if tweet.in_reply_to_status_id is not None:
-            parentTweet = api.get_status(tweet.in_reply_to_status_id)
-            if not parentTweet.retweeted and not (any(keyword in parentTweet.text.lower() for keyword in keywords )) and len(tweet.entities['urls']) > 0:
-                try:
-                    parentTweet.retweet()
-                    logger.info("auto reply...")
-                except Exception as e:
-                    logger.error("Error on retweet", exc_info=True)
+            try:
+                parentTweet = api.get_status(tweet.in_reply_to_status_id)
+
+                if not parentTweet.retweeted and not (any(keyword in parentTweet.text.lower() for keyword in keywords )):
+                    try:
+                        if parentTweet.id not in history:
+                            parentTweet.retweet()
+                            history.add(parentTweet.id)
+                            logger.info("auto reply...")
+                    except Exception as e:
+                        history.add(parentTweet.id)
+                        logger.error("Error on retweet", exc_info=True)
+            except Exception as e:
+                logger.error(e)
         else:
-            if not tweet.retweeted and len(tweet.entities['urls'] > 0):
+            if not tweet.retweeted:
                 try:
-                    tweet.retweet()
+                    if tweet.id not in history:
+                        tweet.retweet()
+                        history.add(tweet.id)
                 except Exception as e:
+                    history.add(tweet.id)
                     logger.error("Error on retweet", exc_info=True)
     return new_since_id
 
 def on_error(self, status):
     logger.error(status)
+    raise Exception("error of cursor")
 
 def main():
     logger.info("start auto reply...")
     api = start_api()
     since_id = 1
     while True:
-        since_id = reply_mentions(api, since_id)
-        logger.info("Waiting...")
-        time.sleep(120)
+        try:
+            since_id = reply_mentions(api, since_id)
+            logger.info("Waiting...")
+            time.sleep(120)
+        except Exception as e:
+            logger.error("Error on main process", e)
+            time.sleep(60)
+            main()
 
 if __name__ == "__main__":
     main()
--- app/retweetFromThirds.py
+++ app/retweetFromThirds.py
@@ -7,6 +7,8 @@ import json
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
+history = set()
+jump = ["localizada", "localización", "localizado"]
 hashtags = ["#IsabelClaudina", "#AlertaIsabelClaudina", "#AlertaAlbaKeneth", "#AlbaKeneth", "#desaparecidosgt", "#botaparecegt"]
 class RetweetFromThird(tweepy.StreamListener):
     def __init__(self, api):
@@ -17,27 +19,36 @@ class RetweetFromThird(tweepy.StreamListener):
         self.api = api
         self.me = api.me()
     def on_status(self, tweet):
-        time.sleep(120)
+        time.sleep(2)
         logger.info(f"Processing tweet with id: {tweet.id}")
         if tweet.in_reply_to_status_id is not None or tweet.user.id == self.me.id:
             return
-        if not tweet.retweeted:
+        if not tweet.retweeted and not any(jumps in tweet.text.lower() for jumps in jump):
             try:
-                tweet.retweet()
+                if tweet.id not in history:                    
+                    tweet.retweet()
+                    history.add(tweet.id)
             except Exception as e:
+                history.add(tweet.id)
                 logger.error("Error on retweet", exc_info=True)
-                time.sleep(300)
+                time.sleep(3)
     
     def on_error(self, status):
         logger.error("Error on retweet ",status)
         time.sleep(3600)
 
 def main(keywords):
-    time.sleep(3600)
-    api = start_api()
-    tweets_listener = RetweetFromThird(api=api)
-    stream = tweepy.Stream(api.auth, tweets_listener)
-    stream.filter(track=keywords)
+    try:
+        time.sleep(3)
+        api = start_api()
+    
+        tweets_listener = RetweetFromThird(api=api)
+        stream = tweepy.Stream(api.auth, tweets_listener)
+        stream.filter(track=keywords)
+    except Exception as e:
+        logger.error(e)
+        time.sleep(3)
+        main(keywords=hashtags)
 
 if __name__ == "__main__":
     main(keywords=hashtags)

0a0197829d2bc45fe8613fbefe961c3425b389a6
Heber <heberdanielorellana@gmail.com>
2021-11-09 02:16:57 +0000

    clean data from twitter

--- app/retweetFromThirds.py
+++ app/retweetFromThirds.py
@@ -27,38 +27,48 @@ class RetweetFromThird(tweepy.StreamListener):
         self.me = api.me()
     def quoted_tweet(self, tweet):
         try:
-            tweetId = tweet.id
-            tweetUser = tweet.user.screen_name
-            getHashtags = self.getHashTag(tweet.text.lower())
-            quotedTweet = "https://twitter.com/{}/status/{}".format(tweetUser, tweetId)
-            tweetText = "Ayúdanos a encontrar esta persona compartiendo esta información {}".format(getHashtags)
-            self.api.update_status(tweetText, attachment_url=quotedTweet)
+            if(not tweet.user.screen_name =="botaparecegt"):
+                tweetId = tweet.id
+                tweetUser = tweet.user.screen_name
+                getHashtags = self.getHashTag(tweet.full_text.lower())
+                quotedTweet = "https://twitter.com/{}/status/{}".format(tweetUser, tweetId)
+                tweetText = "Ayúdanos a encontrar esta persona compartiendo esta información {}".format(getHashtags)
+                self.api.update_status(tweetText, attachment_url=quotedTweet)
+
         except Exception as e:
-            logger.error("")
+            logger.error("error on creating quoted tweet")
+            logging.critical(e,exc_info=True)
+
 
     def on_status(self, tweet):
         #time.sleep(120)
         logger.info(f"Processing tweet with id: {tweet.id}")
         if tweet.in_reply_to_status_id is not None or tweet.user.id == self.me.id:
             return
-        tweetText = tweet.text.lower()
+        full_tweet  = self.api.get_status(tweet.id, tweet_mode='extended')
+        tweetText = full_tweet.full_text.lower()
+
         hasJumps = any(jumps in tweetText for jumps in jump)
+        isMe = tweet.user.screen_name == "botaparecegt"
         isLocated = any(locatedWord in tweetText for locatedWord in locatedKeys)
-        if isLocated:
+        if isLocated and not isMe:
             if not tweetRepo.isLocated(tweet.id):
                 tweetRepo.InsertNewTweet(tweet.id, tweet.created_at)
-        if not tweet.retweeted and not hasJumps and tweet.created_at > since:
+        if not tweet.retweeted and not hasJumps and tweet.created_at > since and not isMe:
             try:
                 if not tweetRepo.isRetweeted(tweet.id):
                     tweetRepo.InsertNewTweet(tweet.id, tweet.created_at)
                     if (tweet.retweeted_status is None):
-                        self.quoted_tweet(tweet)
+                        self.quoted_tweet(full_tweet)
                     else:
                         if(not tweetRepo.isRetweeted(tweet.retweeted_status.id)):
-                            self.quoted_tweet(tweet.retweeted_status)
+                            originalTweet = self.api.get_status(tweet.retweeted_status.id, tweet_mode='extended')
+                            print(originalTweet)
+                            self.quoted_tweet(originalTweet)
                     tweet.retweet()
             except Exception as e:
                 tweetRepo.InsertNewTweet(tweet.id, tweet.created_at)
+                logging.critical(e,exc_info=True)
                 logger.error("Error on retweet", exc_info=True)
                 time.sleep(300)
     
@@ -70,10 +80,16 @@ class RetweetFromThird(tweepy.StreamListener):
 
     def getHashTag(self, tweetText):
         hashtag = ""
+        tweetText = tweetText.replace('.', '')
+        tweetText = tweetText.replace(',', '')
+        tweetText = tweetText.replace('\n', ' ')
         setOfText = tweetText.split(' ')
+        print(setOfText)
         for index in range(len(setOfText)):
             word = setOfText[index]
-            if word in departamentos1:
+            print(word)
+            if any(departamento1 in word for departamento1 in departamentos1):
+                print(hashtag)
                 hashtag = departamentos1[word]
                 break
         if hashtag == "":
@@ -82,8 +98,10 @@ class RetweetFromThird(tweepy.StreamListener):
                 word = setOfText[index]
                 if (index - 1) > 0:
                     wholeWord = setOfText[index -1] + ' ' + word
+                    print(wholeWord)
                     if any(deparamento in wholeWord for deparamento in departamentos2):
                         hashtag = departamentos2[wholeWord]
+                        print(hashtag)
                         break
         
         return hashtag
